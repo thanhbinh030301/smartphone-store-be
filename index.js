2b54a23867936cfa82c56c1258f62ff29c27480c
binhthanh <thanhbinhnguyen097@gmail.com>
2022-06-11 16:45:49 +0700

    update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import cookieParser from 'cookie-parser'
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(bodyParser.json())
