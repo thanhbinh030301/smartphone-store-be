be3ef705b65ed68d43e6ed3b479e5c1c6e380948
binhthanh <thanhbinhnguyen097@gmail.com>
2022-06-11 17:24:05 +0700

    update path

--- routers/products.js
+++ routers/products.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { addProduct, deleteProduct, getProduct, getProductBySlug, updateProduct } from '../controllers/Product.js';
+import { addProduct, deleteProduct, getProduct, getProductBySlug, updateProduct } from '../controllers/product.js';
 const router = express.Router();
 
 router.get('/', getProduct);
